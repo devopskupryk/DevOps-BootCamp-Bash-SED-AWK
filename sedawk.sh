25429db61dfd7d60f34499fb4967ec0230cf73d4
Siarhei Kupryk <siarhei.kupryk@itdep.net>
2022-12-16 14:26:35 +0300

    Version 01

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-# Put you code here
+awk -F: '{OFS=":"; if ($1=="saned") $7="/bin/bash"; print}' ./passwd | \
+awk -F: '!/daemon/ {OFS=":"; print}' | \
+sed '/avahi/s/\/usr\/sbin\/nologin/\/bin\/bash/g' | \
+awk -F: '{OFS=":"; print $1,$3,$5,$7}' | \
+awk -F: '{OFS=":"; if (($2 %2) == 0) $4="/bin/zsh"; print}' > ./passwd_new
